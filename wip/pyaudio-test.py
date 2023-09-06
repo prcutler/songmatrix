01b67df1efca678975f47471de96d2802ee0b59c
Paul Cutler <paul@paulcutler.org>
2023-09-06 10:44:58 -0500

    Update pyaudio test to 30 seconds of recording

--- wip/pyaudio-test.py
+++ wip/pyaudio-test.py
@@ -5,7 +5,7 @@ chunk = 1024  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
 channels = 2
 fs = 44100  # Record at 44100 samples per second
-seconds = 3
+seconds = 30
 filename = "output.wav"
 
 p = pyaudio.PyAudio()  # Create an interface to PortAudio
