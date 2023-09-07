c20824b7ccbdef0404c5ff535996ef855e4709f3
Paul Cutler <paul@paulcutler.org>
2023-09-06 20:47:18 -0500

    Clean up formatting and remove unneeded print statements

--- identify.py
+++ identify.py
@@ -1,8 +1,6 @@
 import asyncio
-
 from Adafruit_IO import Client
 from shazamio import Shazam
-
 import config
 import json
 
@@ -14,15 +12,11 @@ async def main():
     artist = out['track']['subtitle']
     print(track_title + ' by ' + artist)
 
-    # payload_json = {"title": track_title, "artist": artist}
     payload = {"title": track_title, "artist": artist}
     payload_json = json.dumps(payload)
-    # payload = str(payload_json)
-    #payload = track_title + " by " + artist
     print(payload_json)
 
     aio = Client(config.aio_username, config.aio_key)
-    # audio = aio.feeds('audio')
     aio.send_data('audio', payload_json)
 
 loop = asyncio.get_event_loop()
