3911eead42af039f2bb0c77cc6a62830a1f94bb6
Paul Cutler <paul@paulcutler.org>
2023-09-06 20:44:07 -0500

    Update to make song data a JSON object

--- identify.py
+++ identify.py
@@ -4,20 +4,22 @@ from Adafruit_IO import Client
 from shazamio import Shazam
 
 import config
+import json
 
 
 async def main():
     shazam = Shazam()
-    out = await shazam.recognize_song('output.wav')
+    out = await shazam.recognize_song('wip/output.wav')
     track_title = out['track']['title']
     artist = out['track']['subtitle']
     print(track_title + ' by ' + artist)
 
     # payload_json = {"title": track_title, "artist": artist}
-    payload = str({"title": "Summer Girl (Bonus Track)", "artist": "HAIM"})
+    payload = {"title": track_title, "artist": artist}
+    payload_json = json.dumps(payload)
     # payload = str(payload_json)
     #payload = track_title + " by " + artist
-    print(payload)
+    print(payload_json)
 
     aio = Client(config.aio_username, config.aio_key)
     # audio = aio.feeds('audio')
@@ -25,4 +27,3 @@ async def main():
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
-
